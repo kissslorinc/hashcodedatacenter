e054acf88ec2cc2cae42476eaff7ca4bb10a1789
Lorinc Kiss <kissslorinc@hotmail.com>
2017-01-26 15:46:30 +0100

    added slot disabling function in Row

--- src/hashcodedatacenter/Row.java
+++ src/hashcodedatacenter/Row.java
@@ -3,11 +3,17 @@ package hashcodedatacenter;
 import java.util.ArrayList;
 
 public class Row {
+	
+	ArrayList<Slot> slots = new ArrayList<Slot>();
+
 	public Row(int _numberOfSlots) {
 		for (int i = 0; i < _numberOfSlots; i++) {
 			slots.add(new Slot());	
 		}
 	}
+	
+	void disableSlot(int _index) {
+		slots.get(_index).enabled=false;
+	}
 
-	ArrayList<Slot> slots = new ArrayList<Slot>();
 }
