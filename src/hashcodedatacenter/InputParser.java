cb93c6dec39c80bfce5be0b4dd45bf8c40b582be
Lorinc Kiss <kissslorinc@hotmail.com>
2017-01-26 15:20:40 +0100

    Implemented InputParser

--- src/hashcodedatacenter/InputParser.java
+++ src/hashcodedatacenter/InputParser.java
@@ -5,27 +5,37 @@ import java.io.FileReader;
 
 public class InputParser {
 	private enum ReadState {INITREAD,INACTIVEREAD,SERVERREAD};
-	ReadState readState=ReadState.INITREAD;
 	
-	public void read(String path) {
+	public static void read(String path) {
+		ReadState readState=ReadState.INITREAD;
 		try {
-			FileReader fr=new FileReader("testinput.txt");
+			FileReader fr=new FileReader(path);
 			BufferedReader br=new BufferedReader(fr);
 			String[] instr;
-			int maxUnavailable;
-			int maxPools;
+			int maxUnavailable=-1;
+			int numOfServers=-1;
 			while (br.ready()) {
 				instr=br.readLine().split(" ");
 				switch (readState)  {
 				case INITREAD:
 					DataCenter.initializeRows(Integer.parseInt(instr[0]), Integer.parseInt(instr[1]));
+					maxUnavailable=Integer.parseInt(instr[2]);
+					DataCenter.initializePools(Integer.parseInt(instr[3]));
+					numOfServers=Integer.parseInt(instr[4]);
+					 readState=ReadState.INACTIVEREAD;
 					break;
 				case INACTIVEREAD:
+					DataCenter.disableSlot(Integer.parseInt(instr[0]), Integer.parseInt(instr[1]));
+					maxUnavailable--;
+					if (maxUnavailable==0)
+						readState=ReadState.SERVERREAD;
 					break;
 				case SERVERREAD:
+					DataCenter.addServer(Integer.parseInt(instr[0]), Integer.parseInt(instr[1]));
 					break;
 				}
 			}
+			br.close();
 			
 			
 		} catch (Exception e) {
